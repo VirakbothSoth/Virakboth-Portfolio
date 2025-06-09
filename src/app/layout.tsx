11ba18ee6fdb87a42f6a7c027fcc1bb8e04580cc
VirakbothSoth <virakbothsoth@gmail.com>
2025-06-09 21:59:13 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 // src/app/layout.tsx
 export const metadata = {
-  title: 'My Portfolio',
+  title: "My Portfolio",
+  description: 'Welcome to my portfolio website.',
+  keywords: ['portfolio', 'web developer', 'projects', 'Next.js'],
+  authors: [{ name: 'Virakboth Soth' }],
   icons: {
     icon: '/favicon.ico',
   },
-  meta: {
-    title: "Virakboth's Portfolio",
-    description: 'Welcome to my portfolio website.',
-    keywords: ['portfolio', 'web developer', 'projects', 'Next.js'],
-    author: 'Virakboth Soth',
-    viewport: 'width=device-width, initial-scale=1',
-  }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
