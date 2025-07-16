dbf8c59f29c45590650715b249f68b8c5f79096e
Virakboth Soth <virakbothsoth@gmail.com>
2025-07-16 21:08:58 +0700

    /

--- src/app/components/Notes.tsx
+++ src/app/components/Notes.tsx
@@ -4,7 +4,6 @@ import { PiNotepadFill } from "react-icons/pi"
 import { useState } from "react"
 
 const Notes = () => {
-  const [state, setState] = useState(false)
   const [count, setCount] = useState(0)
 
   return (
@@ -29,33 +28,14 @@ const Notes = () => {
             <div className="container mx-auto w-full max-w-4xl px-4">
             <div className="bg-gray-800 rounded-lg w-full" id="a">
               <div className="flex gap-2 px-4 py-2 bg-gray-700 rounded-t-lg">
-                <button
-                  className="w-3 h-3 p-0 m-0 bg-red-500 rounded-full inline-block cursor-pointer"
-                  onClick={() => {
-                    setState(false)
-                  }}
-                  type="button"
-                ></button>
+                <span className="w-3 h-3 bg-red-400 rounded-full inline-block"></span>
                 <span className="w-3 h-3 bg-yellow-400 rounded-full inline-block"></span>
                 <span className="w-3 h-3 bg-green-500 rounded-full inline-block"></span>
               </div>
               <div className="journey-container">
-                { state ? 
-                <div className="pl-3 journey-container-alt">
-                  <p>I'm a 14 year old developer from <b>Cambodia</b> that can make websites & simple programs.</p>
-                  <br></br>
-                  <p>My Hobbies are..</p>
-                  <ul className="pl-5 list-disc">
-                    <li><p>🧑‍💻 Coding and Developing (duh.)</p></li>
-                    <li><p>🫂 Talking and meeting friends</p></li>
-                    <li><p>🎮 Gaming</p></li>
-                  </ul>
-                </div>
-                : 
                 <div className="pl-3 journey-container-alt">
                   <textarea className="w-full text-xl"></textarea>
                 </div>
-              }
               </div>
             </div>
           </div>
@@ -64,7 +44,7 @@ const Notes = () => {
             <h3 className="text-3xl font-bold mb-[50px]">Clicker</h3>
             <div className="bg-gray-800 rounded-lg">
               <div className="journey-container">
-                <div className="pl-3">
+                <div>
                   <p>
                     { count }
                   </p>
