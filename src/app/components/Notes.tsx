5a446741394f031ebeb4d7b69a8a915bc9dbf84d
Virakboth Soth <virakbothsoth@gmail.com>
2025-08-10 21:10:44 +0700

    made a code tab i guess

--- src/app/components/Notes.tsx
+++ src/app/components/Notes.tsx
@@ -15,7 +15,7 @@ const Notes = () => {
         </h2>
         <div className="text-left flex flex-wrap justify-center gap-6">
           <div className="note-card">
-            <h3 className="text-3xl font-bold mb-[50px]">thoughts.txt</h3>
+            <h3 className="text-3xl font-bold mb-[50px]">Thoughts</h3>
             <div className="bg-gray-800 rounded-lg">
               <div className="journey-container">
                 <div className="pl-3">
@@ -28,7 +28,7 @@ const Notes = () => {
             </div>
           </div>
           <div className="note-card">
-            <h3 className="text-3xl font-bold mb-[50px]">tab.md</h3>
+            <h3 className="text-3xl font-bold mb-[50px]">Tab 1</h3>
             <div className="container mx-auto w-full max-w-4xl px-4">
               <div className="bg-gray-800 rounded-lg w-full" id="a">
                 <div className="flex gap-2 px-4 py-2 bg-gray-700 rounded-t-lg">
@@ -84,6 +84,19 @@ const Notes = () => {
               >
                 {state ? "shut up" : "nvm"}
               </button>
+            </div>
+          </div>
+          <div className="note-card">
+            <h3 className="text-3xl font-bold mb-[50px]">Code Tab 1</h3>
+            <div className="bg-gray-800 rounded-lg">
+              <div className="journey-container">
+                <div className="pl-3 codemock">
+                  <p>
+                    <b>print</b>&#40;<b className="text-green-300">"Hello Worl</b>
+                    <div className="animate-blink inline">&#9608;</div>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
